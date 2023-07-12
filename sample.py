06b7a89730742b6c4482d9a4bd7e9960def92438
Henry Bazan <109365110+HenryBazan@users.noreply.github.com>
2023-07-12 02:07:25 +0000

    opperations

--- sample.py
+++ sample.py
@@ -1,3 +1,11 @@
 #Testing adding a python program
 
 print("Hello! are you also tryihg to learn about git?")
+
+#Test was successful, I was able to create a py file and add code.
+
+#second test is to add some opperations. with variables.
+
+num1=5
+num2=7
+print(num1 + num2)
