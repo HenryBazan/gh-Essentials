1dc1a8aec1094b5b8976e192d530bb1746e55905
Henry Bazan <109365110+HenryBazan@users.noreply.github.com>
2023-07-12 02:19:14 +0000

    second test

--- sample.py
+++ sample.py
@@ -9,3 +9,11 @@ print("Hello! are you also tryihg to learn about git?")
 num1=5
 num2=7
 print(num1 + num2)
+
+#adding some complexity, but not really.
+#Writting int(input()) to ensure variable is an integer.
+num4=int(input("Please enter a number."))
+num5=int(input("Now insert a second number."))
+
+#f string to allow the values of variable to be displayed instead of the variable name.
+print(f"{num4} + {num5} = {num4+num5}")
